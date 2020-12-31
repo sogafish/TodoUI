d4501244464fb64a0ff94de08cf1ef5cd69d09b3
sogafish <k.sogafish@gmail.com>
2020-12-31 23:29:13 +0900

    :tada:

---
+++ TodoUI/TodoUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoUIApp.swift
+//  TodoUI
+//
+//  Created by k.soga on 2020/12/31.
+//
+
+import SwiftUI
+
+@main
+struct TodoUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
